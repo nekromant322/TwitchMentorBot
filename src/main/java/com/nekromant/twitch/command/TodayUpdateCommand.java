a30190541eda1751ec2cfbc6f9aaed479895d5f9
Mariya Balandina <mariya.balda@gmail.com>
2023-03-30 20:43:57 +0300

    Changed checking permission with throwning exception

--- src/main/java/com/nekromant/twitch/command/TodayUpdateCommand.java
+++ src/main/java/com/nekromant/twitch/command/TodayUpdateCommand.java
@@ -1,8 +1,9 @@
 package com.nekromant.twitch.command;
 
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
-import com.nekromant.twitch.contant.Message;
 import com.nekromant.twitch.ModerationTwitchHelix;
+import com.nekromant.twitch.contant.Message;
+import com.nekromant.twitch.exception.PermissionException;
 import com.nekromant.twitch.service.ReviewScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,8 +13,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.nekromant.twitch.contant.MessageContent.NO_PERMISSION;
 import static com.nekromant.twitch.contant.MessageContent.TODAY_UPDATE_CONFIRM;
-import static com.nekromant.twitch.contant.MessageContent.TODAY_UPDATE_REJECT;
 
 @Component
 public class TodayUpdateCommand extends BotCommand {
@@ -34,20 +35,19 @@ public class TodayUpdateCommand extends BotCommand {
     public void processMessage(ChannelMessageEvent event) {
         String channelName = event.getChannel().getName();
         String senderUsername = event.getMessageEvent().getUser().getName();
-        String broadcasterId = event.getChannel().getId();
-        List<String> moderators = getModeratorList(broadcasterId);
-
         Message replyMessage = new Message(senderUsername, "");
 
-        if (senderUsername.equals(channelOwner) || moderators.contains(senderUsername)) {
+        try {
+            checkPermission(senderUsername, event);
             String message = event.getMessage();
             String newReviewSchedule = Arrays.stream(message.split(" ")).skip(1).collect(Collectors.joining(" "));
 
             reviewScheduleService.saveSchedule(newReviewSchedule);
             replyMessage.setMessageText(TODAY_UPDATE_CONFIRM);
-        } else {
-            replyMessage.setMessageText(TODAY_UPDATE_REJECT);
+        } catch (Exception e) {
+            replyMessage.setMessageText(NO_PERMISSION);
         }
+
         event.getTwitchChat().sendMessage(channelName, replyMessage.getMessage());
     }
 
@@ -60,5 +60,14 @@ public class TodayUpdateCommand extends BotCommand {
                 .collect(Collectors.toList());
 
         return moderators;
+    }
+
+    public void checkPermission(String senderUsername, ChannelMessageEvent event) {
+        String broadcasterId = event.getChannel().getId();
+        List<String> moderators = getModeratorList(broadcasterId);
+
+        if (!senderUsername.equals(channelOwner) || !moderators.contains(senderUsername)) {
+            throw new PermissionException();
+        }
     }
 }
--- src/main/java/com/nekromant/twitch/contant/MessageContent.java
+++ src/main/java/com/nekromant/twitch/contant/MessageContent.java
@@ -5,6 +5,6 @@ public class MessageContent {
     public static final String TODAY_REVIEW_HEADER = "Расписание ревью на сегодня\n | \n";
     public static final String NO_REVIEWS = "На данный момент нет назначенных ревью";
     public static final String TODAY_UPDATE_CONFIRM = "Расписание обновлено";
-    public static final String TODAY_UPDATE_REJECT = "Ты не можешь менять расписание";
+    public static final String NO_PERMISSION = "Тебе недоступна эта команда";
 
 }
