ac786551f60da697a1dcc8591815972583cda643
nekromant322 <shamaneeel881@gmail.com>
2023-04-03 22:43:24 +0300

    edited today header

--- src/main/java/com/nekromant/twitch/contant/MessageContent.java
+++ src/main/java/com/nekromant/twitch/contant/MessageContent.java
@@ -2,7 +2,7 @@ package com.nekromant.twitch.contant;
 
 public class MessageContent {
 
-    public static final String TODAY_REVIEW_HEADER = "Расписание ревью на сегодня\n | \n";
+    public static final String TODAY_REVIEW_HEADER = "Расписание на сегодня\n | \n";
     public static final String NO_REVIEWS = "На данный момент нет назначенных ревью";
     public static final String TODAY_UPDATE_CONFIRM = "Расписание обновлено";
     public static final String NO_PERMISSION = "Тебе недоступна эта команда";
