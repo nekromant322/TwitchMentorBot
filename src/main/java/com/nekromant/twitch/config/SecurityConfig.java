e25ecd3432808ec70fb736694420d3f207dc5740
etozhealexis <alexisworksya@yandex.ru>
2023-04-23 01:09:58 +0300

    security works

--- src/main/java/com/nekromant/twitch/config/SecurityConfig.java
+++ src/main/java/com/nekromant/twitch/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.nekromant.twitch.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +17,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        try {
+            auth.inMemoryAuthentication()
+                    .withUser("admin")
+                    .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                    .roles("ADMIN");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/twitch-bot/**").authenticated()
+                .antMatchers("/twitch-bot/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and().formLogin();
-//                .anyRequest().permitAll();
     }
 }
