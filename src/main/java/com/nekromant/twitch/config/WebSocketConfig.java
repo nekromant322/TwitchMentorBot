056c3e867d9fe1fa4f4426a95a28a30b9663e064
etozhealexis <alexisworksya@yandex.ru>
2023-04-25 21:26:57 +0300

    initial 2

--- src/main/java/com/nekromant/twitch/config/WebSocketConfig.java
+++ src/main/java/com/nekromant/twitch/config/WebSocketConfig.java
@@ -1,4 +1,17 @@
 package com.nekromant.twitch.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
 }
---
+++ src/main/java/com/nekromant/twitch/contoller/rest/WebSocketController.java
@@ -0,0 +1,8 @@
+package com.nekromant.twitch.contoller.rest;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+}
