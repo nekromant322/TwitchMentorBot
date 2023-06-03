9855403a6f124d86ec28d8fda81d82dc7ed93cf2
79510 <eduard-proskurin@mail.ru>
2023-06-03 20:11:59 +0300

    Добавлен метод, который получает список команды, где поле period не равно входящему значению, поле Enabled = true

--- src/main/java/com/nekromant/twitch/repository/TwitchCommandRepository.java
+++ src/main/java/com/nekromant/twitch/repository/TwitchCommandRepository.java
@@ -4,7 +4,11 @@ import com.nekromant.twitch.model.TwitchCommand;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TwitchCommandRepository extends CrudRepository<TwitchCommand, Long> {
     TwitchCommand findByName(String name);
+
+    List<TwitchCommand> findAllByPeriodNotAndEnabledIsTrue(Long period);
 }
