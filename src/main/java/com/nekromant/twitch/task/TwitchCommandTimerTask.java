00744ad1ee35e490dcc2b08323442e977d6f6082
79510 <eduard-proskurin@mail.ru>
2023-06-07 18:32:47 +0300

    получение twitchClient теперь выполняется через twitchClientHolder

--- src/main/java/com/nekromant/twitch/task/TwitchCommandTimerTask.java
+++ src/main/java/com/nekromant/twitch/task/TwitchCommandTimerTask.java
@@ -1,6 +1,6 @@
 package com.nekromant.twitch.task;
 
-import com.github.twitch4j.TwitchClient;
+import com.nekromant.twitch.TwitchClientHolder;
 import com.nekromant.twitch.model.TwitchCommand;
 import com.nekromant.twitch.service.TwitchAuthService;
 
@@ -8,13 +8,14 @@ import java.util.Collections;
 
 
 public class TwitchCommandTimerTask implements Runnable {
-    private final TwitchClient twitchClient;
+    private final TwitchClientHolder twitchClientHolder;
     private final String channelName;
     private final TwitchCommand twitchCommand;
     private final TwitchAuthService twitchAuthService;
 
-    public TwitchCommandTimerTask(TwitchClient twitchClient, String channelName, TwitchCommand twitchCommand, TwitchAuthService twitchAuthService) {
-        this.twitchClient = twitchClient;
+    public TwitchCommandTimerTask(TwitchClientHolder twitchClientHolder, String channelName, TwitchCommand twitchCommand,
+                                  TwitchAuthService twitchAuthService) {
+        this.twitchClientHolder = twitchClientHolder;
         this.channelName = channelName;
         this.twitchCommand = twitchCommand;
         this.twitchAuthService = twitchAuthService;
@@ -23,13 +24,14 @@ public class TwitchCommandTimerTask implements Runnable {
     @Override
     public void run() {
         if (isLiveStream()) {
-            twitchClient.getChat().sendMessage(channelName, twitchCommand.getResponse());
+            twitchClientHolder.getTwitchClient().getChat().sendMessage(channelName, twitchCommand.getResponse());
         }
     }
 
     private boolean isLiveStream() {
-        return !twitchClient.getHelix().getStreams(twitchAuthService.getAuthToken().getAccessToken(), null,
-                        null, 1, null, null, null, Collections.singletonList(channelName))
+        return !twitchClientHolder.getTwitchClient().getHelix().getStreams(twitchAuthService.getAuthToken()
+                                .getAccessToken(), null, null, 1, null, null, null,
+                        Collections.singletonList(channelName))
                 .execute().getStreams().isEmpty();
     }
 }
