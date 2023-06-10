af948adca79ba7832ca4ea266175af701c4dd2cb
79510 <eduard-proskurin@mail.ru>
2023-06-10 13:45:52 +0300

    Убран неиспользуемый метод

--- src/main/java/com/nekromant/twitch/repository/TwitchUserMessageRepository.java
+++ src/main/java/com/nekromant/twitch/repository/TwitchUserMessageRepository.java
@@ -3,8 +3,6 @@ package com.nekromant.twitch.repository;
 import com.nekromant.twitch.model.TwitchUserMessage;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface TwitchUserMessageRepository extends CrudRepository<TwitchUserMessage, Long> {
-    List<TwitchUserMessage> getTwitchUserMessageByName(String name);
+
 }
