b0c61e153c622fb8f559af43d98b9df07b532816
79510 <eduard-proskurin@mail.ru>
2023-06-11 19:01:37 +0300

    Убран метод findByName

--- src/main/java/com/nekromant/twitch/repository/TwitchUserRepository.java
+++ src/main/java/com/nekromant/twitch/repository/TwitchUserRepository.java
@@ -1,10 +1,7 @@
 package com.nekromant.twitch.repository;
 
 import com.nekromant.twitch.model.TwitchUser;
-import com.nekromant.twitch.model.TwitchUserMessage;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TwitchUserRepository extends CrudRepository<TwitchUser, Long> {
-
-    TwitchUser findByName(String name);
 }
