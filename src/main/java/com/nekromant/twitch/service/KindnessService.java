9153e246175b71af47a1e9ff5fc6ee3b6cc5a6e2
79510 <eduard-proskurin@mail.ru>
2023-06-16 20:25:37 +0300

    Рефакторинг

--- src/main/java/com/nekromant/twitch/service/KindnessService.java
+++ src/main/java/com/nekromant/twitch/service/KindnessService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -45,7 +44,6 @@ public class KindnessService {
         evaluationKindnessUser(twitchUserService.getTwitchUserWithMostMessages());
     }
 
-    @Transactional
     public void evaluationKindnessUser(TwitchUser twitchUser) {
         if (twitchUser != null) {
             String twitchUserMessages = twitchUser.getMessages().stream()
