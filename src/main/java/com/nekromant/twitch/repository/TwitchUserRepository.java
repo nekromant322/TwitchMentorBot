5c29e4e832728b42ba945aaf067df0e374419f02
nekromant322 <shamaneeel881@gmail.com>
2023-06-18 00:13:04 +0300

    Revert "Добавлен метод, который находит пользователя с наибольшим количеством сообщений"
    This reverts commit 376a52a5fb8241de92757a6e1224dbb087356003.

--- src/main/java/com/nekromant/twitch/repository/TwitchUserRepository.java
+++ src/main/java/com/nekromant/twitch/repository/TwitchUserRepository.java
@@ -1,11 +1,7 @@
 package com.nekromant.twitch.repository;
 
 import com.nekromant.twitch.model.TwitchUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TwitchUserRepository extends JpaRepository<TwitchUser, Long> {
-    @Query(value = "SELECT u FROM twitch_users u WHERE size(u.messages) " +
-            "= (SELECT MAX(size(u2.messages)) FROM twitch_users u2)")
-    TwitchUser findTwitchUserWithMaxMessages();
+public interface TwitchUserRepository extends CrudRepository<TwitchUser, Long> {
 }
