bad6531f6e866233b306a6c39036e3e8fb75bb9f
Anton Kleban <anton.kleban.98@yandex.ru>
2023-07-30 15:15:25 +0300

    -

---
+++ src/main/java/com/nekromant/twitch/command/SongListCommand.java
@@ -0,0 +1,25 @@
+package com.nekromant.twitch.command;
+
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.nekromant.twitch.content.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SongListCommand extends BotCommand{
+    @Value("${twitch.song-list.songListRedirectUrl}")
+    String songListUrl;
+
+    public SongListCommand() {
+        super("sl", "Отправляет ссылку на song list");
+    }
+
+    @Override
+    public void processMessage(ChannelMessageEvent event) {
+        String channelName = event.getChannel().getName();
+        String senderUsername = event.getMessageEvent().getUser().getName();
+
+        Message replyMessage = new Message(senderUsername, songListUrl);
+        event.getMessageEvent().getTwitchChat().sendMessage(channelName, replyMessage.getMessage());
+    }
+}
